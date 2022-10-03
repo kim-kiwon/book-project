6b7aa6653954f70cf372dca0fcd4cfdd08c32d59
kim-kiwon <kimkiwonn@gmail.com>
2022-10-04 00:46:59 +0900

    feat: dto 테스트 추가

--- src/main/java/com/springboot/book/boot/web/HelloController.java
+++ src/main/java/com/springboot/book/boot/web/HelloController.java
@@ -3,10 +3,17 @@ package com.springboot.book.boot.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.book.boot.web.dto.HelloResponseDto;
+
 @RestController
 public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+
+	@GetMapping("/hello/dto")
+	public HelloResponseDto helloDto(String name, int amount) {
+		return new HelloResponseDto(name, amount);
 	}
 }

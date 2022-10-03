66c86360615b8c068b76531e350c4752abe49b47
kim-kiwon <kimkiwonn@gmail.com>
2022-10-04 00:38:40 +0900

    feat: lombok 추가 및 기본 테스트 추가

---
+++ src/main/java/com/springboot/book/boot/Application.java
@@ -0,0 +1,9 @@
+package com.springboot.book.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+}

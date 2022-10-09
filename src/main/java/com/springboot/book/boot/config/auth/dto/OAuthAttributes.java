79cb637a652ab9acbb58e3c9d4f03714b81012ac
kim-kiwon <kimkiwonn@gmail.com>
2022-10-10 01:30:40 +0900

    feat: naver 로그인 기능 추가

--- src/main/java/com/springboot/book/boot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/book/boot/config/auth/dto/OAuthAttributes.java
@@ -25,10 +25,24 @@ public class OAuthAttributes { // OAuth2UserService를 통해 가져온 OAuth2Us
 		this.picture = picture;
 	}
 
-	public static OAuthAttributes of(String registrationId, String userNameAttributesName, Map<String, Object> attributes) {
-		return ofGoogle(userNameAttributesName, attributes);
+	public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+		if("naver".equals(registrationId)) {
+			return ofNaver("id", attributes);
+		}
+
+		return ofGoogle(userNameAttributeName, attributes);
 	}
 
+	private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+		return OAuthAttributes.builder()
+			.name((String) response.get("name"))
+			.email((String) response.get("email"))
+			.picture((String) response.get("profile_image"))
+			.attributes(response)
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
 	private static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String, Object> attributes) {
 		return OAuthAttributes.builder()
 			.name((String) attributes.get("name"))
